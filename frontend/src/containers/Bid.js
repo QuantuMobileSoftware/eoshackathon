d5fff6dce761bf55d595dc986125f7ef9118d428
Aleksandr Dolgaryev <adolgarev@gmail.com>
2018-06-09 14:27:01 +0300

    Get rid of getElementById

--- frontend/src/containers/Bid.js
+++ frontend/src/containers/Bid.js
@@ -2,29 +2,57 @@ import React, {Component, Fragment} from 'react';
 import Eos from "eosjs";
 
 export class Bid extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            amount: 0,
+            price: 0,
+            pk: "",
+            accountName: ""
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
     render() {
         return (
                 <Fragment>
-                        <input type="number" id="amount" placeholder="0"/>
-                        <input type="number" id="price" placeholder="0"/>
-                        <input type="text" id="pk" placeholder="pk"/>
-                        <input type="text" id="name" placeholder="Name"/>
+                        <input type="number" name="amount" value={this.state.amount} onChange={this.handleInputChange} />
+                        <input type="number" name="price" value={this.state.price} onChange={this.handleInputChange} />
+                        <input type="text" name="pk" placeholder="pk" value={this.state.pk} onChange={this.handleInputChange} />
+                        <input type="text" name="accountName" placeholder="Account name" value={this.state.accountName} onChange={this.handleInputChange} />
                         <input type="button" onClick={this.handleClickBuy} value="Buy"/>
                         <input type="button" onClick={this.handleClickSell} value="Sell"/>
                 </Fragment>
                 );
     }
 
+    handleInputChange = (event) => {
+        const target = event.target;
+        var value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        
+        if (name === "amount" || name === "price") {
+            value = parseInt(value);
+        }
+
+        this.setState({
+            [name]: value
+        });
+    }
+
     handleClickBuy = () => {
         const eos = Eos({
             httpEndpoint: 'http://' + window.location.hostname + ':8888',
-            keyProvider: document.getElementById('pk').value,
+            keyProvider: this.state.pk,
             debug: true
         });
-        const amount = document.getElementById("amount").value;
-        const price = document.getElementById("price").value;
-        const name = document.getElementById("name").value;
-        const data = {bidder: name, bidType: 1, price: parseInt(price), amount: parseInt(amount)};
+        const data = {
+            bidder: this.state.accountName,
+            bidType: 0,
+            price: this.state.price,
+            amount: this.state.amount
+        };
         eos.transaction({
             actions: [
                 {
@@ -43,13 +71,15 @@ export class Bid extends Component {
     handleClickSell = () => {
         const eos = Eos({
             httpEndpoint: 'http://' + window.location.hostname + ':8888',
-            keyProvider: document.getElementById('pk').value,
+            keyProvider: this.state.pk,
             debug: true
         });
-        const amount = document.getElementById("amount").value;
-        const price = document.getElementById("price").value;
-        const name = document.getElementById("name").value;
-        const data = {bidder: name, bidType: 1, price: parseInt(price), amount: parseInt(amount)};
+        const data = {
+            bidder: this.state.accountName,
+            bidType: 1,
+            price: this.state.price,
+            amount: this.state.amount
+        };
         eos.transaction({
             actions: [
                 {
@@ -66,6 +96,7 @@ export class Bid extends Component {
     }
 
     componentDidMount() {
+        var self = this;
         const controlWebsocket = new WebSocket("ws://" + window.location.hostname + ":1337/");
         controlWebsocket.onopen = function () {
         };
