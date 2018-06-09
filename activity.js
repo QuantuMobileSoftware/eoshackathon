a0c767721d9a4ebf6619dfea1c3c12af7a7ab7c0
igorkorotach <igorkorotach@gmail.com>
2018-06-10 01:23:02 +0800

    activity bot, needs testing on plots

---
+++ activity.js
@@ -0,0 +1,84 @@
+const Eos = require("eosjs");
+
+const eos = Eos({
+    httpEndpoint: 'http://localhost:8888',
+    keyProvider: "5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3",
+    debug: true
+});
+
+
+function randomIntInc(low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low);
+}
+
+function provideTranasction(arr) {
+    for (let item of arr) {
+        const data_bid = {
+            bidder: "testers",
+            bidType: 0,
+            price: item[0],
+            amount: 20
+        };
+        eos.transaction({
+            actions: [
+                {
+                    account: 'decidex',
+                    name: "placebid",
+                    authorization: [{
+                        actor: 'decidex',
+                        permission: 'active'
+                    }],
+                    data: data_bid
+                }
+            ]
+        }).then(result => console.log(result));
+        const data_ask = {
+            bidder: "testers",
+            bidType: 1,
+            price: item[1],
+            amount: 20
+        };
+        eos.transaction({
+            actions: [
+                {
+                    account: 'decidex',
+                    name: "placebid",
+                    authorization: [{
+                        actor: 'decidex',
+                        permission: 'active'
+                    }],
+                    data: data_ask
+                }
+            ]
+        }).then(result => console.log(result));
+    }
+}
+
+function getRandIter() {
+    let arr = [];
+    switch (randomIntInc(1, 3)) {
+        case 1:
+            for (let i = 0; i < 10; i++) {
+                arr.push([randomIntInc(50, 60), randomIntInc(38, 50)])
+            }
+            console.log(arr);
+            provideTranasction(arr);
+            return;
+        case 2:
+            for (let i = 0; i < 10; i++) {
+                arr.push([randomIntInc(4, 8), randomIntInc(1, 4)])
+            }
+            console.log(arr);
+            provideTranasction(arr);
+            return;
+        case 3:
+            for (let i = 0; i < 10; i++) {
+                arr.push([randomIntInc(15, 20), randomIntInc(14, 15)])
+            }
+            console.log(arr);
+            provideTranasction(arr);
+            return;
+    }
+}
+
+setInterval(getRandIter, 10000);
